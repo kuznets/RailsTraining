f82d079d5e3d2718876cf3e78c21fe7b61eb30d9
kuznets <1sttech.eu@gmail.com>
2017-02-02 18:06:04 +0200

    The base guide implementation

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'web_app/index'
+
+  resources :articles do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'web_app#index'
 end
